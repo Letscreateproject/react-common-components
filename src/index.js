b0c1ef724aca8d249fac8b8986f532d414d6748a
akshay <akshay.rm@bourntec.com>
2024-02-14 12:31:50 +0530

    login page implementation

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider , createBrowserRouter} from 'react-router-dom';
+import SideMenu from './components/SideMenu';
+import LoginPage from './components/login/login';
+
+/**
+ * Defining the routing path and components
+ */
+const router = createBrowserRouter([
+  {path:'', element:<LoginPage/>},
+  {path:'/home', element:<SideMenu/>}
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
