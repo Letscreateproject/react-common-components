f46583b6b527f972e2abf49f2046eaeaf0045fe7
Sarath G Krishnan <sarath.krishnan@bourntec.com>
2024-02-23 11:14:45 +0530

    style change

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -6,7 +6,7 @@ import image from '../assets/success.png'
 function Success() {
   return (
     <div style={{marginTop:'115px'}}>
-     <img style={{width:'60px', marginLeft:'480px'}} src={image} alt=" Empty page" />
+     <img style={{width:'60px', marginLeft:'650px'}} src={image} alt=" Empty page" />
       <h1 style={{textAlign:'center'}}>Payment Done Successfully</h1>
       <div className="proceed-button">
             <button type="button" className='button'>DOWNLOAD INVOICE</button>
